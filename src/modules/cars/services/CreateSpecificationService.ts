5e5666cb7819277e4be446f7f691a8c2d4623c57
Junior de Oliveira <ellismarjr@hotmail.com>
2021-09-21 08:42:30 -0300

    chore: not allowed create specification if specification name already exists

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -9,6 +9,13 @@ class CreateSpecificationService {
   constructor(private specificationRepository: ISpecificationsRepository) {}
 
   execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error('Specification already exists');
+    }
+
     this.specificationRepository.create({ name, description });
   }
 }
