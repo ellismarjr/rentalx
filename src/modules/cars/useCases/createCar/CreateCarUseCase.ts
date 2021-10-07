809544301e45c2666c6288061a9810d8a5200229
Junior de Oliveira <ellismarjr@hotmail.com>
2021-10-07 15:54:14 -0300

    tests: should not be able to create a new car with exists license plate

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -27,6 +27,13 @@ class CreateCarUseCase {
     brand,
     category_id,
   }: IRequest): Promise<void> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate,
+    );
+    if (carAlreadyExists) {
+      throw new Error('Car already exists');
+    }
+
     await this.carsRepository.create({
       name,
       description,
